35c07493acad79468f90cf6501ee7551a5c8a306
Taylor Roy <u6012349@hci.utah.edu>
2020-03-12 17:51:05 -0600

    scroll, styling, table

--- src/static/tumorInfo.js
+++ src/static/tumorInfo.js
@@ -1,4 +1,4 @@
-//javascript to handle button click, get sample name, and add to list
+//javascript to handle select button click, get sample name, and add to list
 var idText;
 var idTextArray = [];
 var cost = 0;
@@ -6,27 +6,36 @@ var count = 0;
 $(document).ready(function () {
 
     $(this).on("click", function () {
+       
         //make submit button active if a sample has been selected
         document.getElementById("submitOrder").disabled = false;
         //get id value for button that was clicked(this)
         idText = this.activeElement.id;
-
+        
         //check if idText is in idTextArray.  If -1 then not in array.
         var repeat = idTextArray.indexOf(idText);
 
-        //create a RegExp for the search term. Example /o/ig 
-        var searchExp = new RegExp(/hci/, "ig");
+        //create a RegExp for the search term. Samples had id with "HCI" and "TOW" in name . Output example /o/ig 
+        var searchHCI = new RegExp(/hci/, "ig");
+        var searchTOW = new RegExp(/tow/, "ig");
+
 
         //test if search term is true in element text in html
-        var match = searchExp.test(idText);
+        var matchHCI = searchHCI.test(idText);
+        var matchTOW = searchTOW.test(idText);
+        
 
-        if (match && repeat < 0) {
+        if ((matchHCI || matchTOW) && repeat < 0) {
             idTextArray.push(idText);
             console.log(idTextArray);
             cost += 750;
-            //dynamically adds a <li> and <button> to <ol> 
+            //dynamically adds a <div> <li> and <button> to <ol> 
             $("ol").append('<div id="listDiv" class=' + idText + '><li class="listItem id="orderList" >' + idText + '</li><p class="">$750.00</p><button class="btn btn-danger" id ="remove" data-data=' + idText + '>X</button></div>');
             $(".cost").text("$" + cost);
+            document.getElementById(`${idText}`).disabled= true;
+            document.getElementById(`${idText}`).className= "btn btn-danger";
+            document.getElementById(`${idText}`).innerHTML = "In Cart";
+            
             count++;
         }
         //disable submit button if all samples were removed from order
@@ -35,9 +44,6 @@ $(document).ready(function () {
         }
     });
 });
-
-
-
 
 //javascript to handle submit button click, and send email with order.
 $(document).ready(function () {
@@ -85,16 +91,26 @@ $(document).ready(function () {
     })
 });
 
-
 //javascript to handle remove button click.  Also allows removed samples to be readded if desired.
 var buttondData;
 var removeIndex;
 $(document).ready(function () {
     $(".tumorOrderSummary").on("click", "#remove", function () {
+        
         buttondData = this.dataset.data;
+        
+        //enables "select" button for samples if removed from cart
+        document.getElementById(`${buttondData}`).disabled= false;
+        document.getElementById(`${buttondData}`).className= "btn btn-success";
+        document.getElementById(`${buttondData}`).innerHTML = "Select";
+
+        //removes <div> with sample in cart
         $(`.${buttondData}`).remove();
         removeIndex = idTextArray.indexOf(buttondData)
+        
+        //removes id from array that keeps track of what has been selected
         idTextArray.splice(removeIndex, 1);
+
         //subtracts from total if sample is removed
         cost -= 750;
         $(".cost").text("$" + cost);
@@ -113,7 +129,7 @@ $(document).ready(function () {
 
 //disable submit button initially because no samples have been selected
 $(document).ready(function () {
-    console.log(idTextArray.length);
+    
     if (idTextArray.length === 0) {
         document.getElementById("submitOrder").disabled = true;
     }
